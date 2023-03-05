a1c13a39ea6f2e1efa786c586f78953f3e469bd4
Aleksandr <zaytsev.chel@gmail.com>
2023-03-05 18:09:40 +0500

    Домашка

---
+++ Example003/Program.cs
@@ -0,0 +1,61 @@
+﻿// Например, изначально массив
+// выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Новый массив будет выглядеть
+// вот так:
+// 1 4 7 2
+// 5 81 2 9
+// 8 4 2 4
+
+
+int ReadInt(string text)
+{
+System.Console.Write(text);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] CreateMatrix(int m, int n)
+{
+int[,] matrix = new int [m, n];
+    for(int i = 0; i < m; i++)
+    {
+     for(int j = 0; j < n; j++)
+        {
+            matrix[i,j] = new Random().Next(-10, 10);
+        }
+    }
+return matrix;
+}
+
+void NewMatrix (int[,] matrix)
+{
+    for (int i = 1; i < matrix.GetLength(0); i+=2)
+    {
+        for (int j = 1; j < matrix.GetLength(1); j+=2)
+        {
+            matrix[i,j] *= matrix[i,j]; 
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+        System.Console.Write(matrix[i,j] + "\t");;
+        }
+        System.Console.WriteLine();
+        }
+System.Console.WriteLine();
+}
+
+int m = ReadInt("Введите количетсво строк матрицы: ");
+int n = ReadInt("Введите количество столбцов матрицы: ");
+int [,] matrix = CreateMatrix(m,n);
+PrintMatrix(matrix);
+NewMatrix(matrix);
+PrintMatrix(matrix);
---
+++ Example004/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+int ReadInt(string text)
+{
+System.Console.Write(text);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] CreateMatrix(int m, int n)
+{
+int[,] matrix = new int [m, n];
+    for(int i = 0; i < m; i++)
+    {
+     for(int j = 0; j < n; j++)
+        {
+            matrix[i,j] = new Random().Next(-10, 10);
+        }
+    }
+return matrix;
+}
+
+int Number(int[,] matrix)
+{
+    int number = 0;
+    for (int i = 0; i < matrix.GetLength(0) && i < matrix.GetLength(1); i++)
+    {
+        number += matrix[i,i];
+    }
+    return number;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+        System.Console.Write(matrix[i,j] + "\t");;
+        }
+        System.Console.WriteLine();
+        }
+System.Console.WriteLine();
+}
+
+
+
+int m = ReadInt("Введите количетсво строк матрицы: ");
+int n = ReadInt("Введите количество столбцов матрицы: ");
+int [,] matrix = CreateMatrix(m,n);
+PrintMatrix(matrix);
+
+
+System.Console.WriteLine("Сумма элементов главной диагонали: " + Number(matrix));
